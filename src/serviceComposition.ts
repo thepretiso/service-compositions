e919ab18ddbe64553751205943a659f81cd4c775
thepretiso <pretak.m@seznam.cz>
2024-08-20 12:21:28 +0200

    feat: working first path

--- src/serviceComposition.ts
+++ src/serviceComposition.ts
@@ -13,8 +13,64 @@ export class ServiceComposition {
         private errorMapper: ErrorMapper,
     ){}
 
-    run(req: Main_Request, timeoutMillis: number, cancellation: CancellationToken): Promise<Main_Result> {
-        throw new Error("TODO your implementation here")
+    async run(req: Main_Request, timeoutMillis: number, cancellation: CancellationToken): Promise<Main_Result> {
+        const errors: Error[] = [];
+        let completed = false;
+        let timeoutHandler: NodeJS.Timeout | undefined = undefined
+
+        const timeoutPromise = new Promise((_, reject) => {
+            timeoutHandler = setTimeout(() => {
+                completed = false
+                reject(this.errorMapper.timedOut());
+            }, timeoutMillis);
+        });
+
+        cancellation.onCancelled(() => {
+            clearTimeout(timeoutHandler);
+            throw this.errorMapper.aborted();
+        })
+
+        try {
+            const tokenServiceA = this.a.start(req)
+            const pollServiceA = async (): Promise<ServiceA_Result> => {
+                while (!cancellation.isCancelled()) {
+                    const result = this.a.poll(tokenServiceA);
+                    if (result !== null) return result;
+                }
+                this.a.abort(tokenServiceA);
+                throw this.errorMapper.aborted();
+            };
+
+            const serviceB = new Promise<ServiceB_Result>((resolve, reject) => {
+                this.b.submit(
+                    req,
+                    () => cancellation.isCancelled(),
+                    timeoutMillis,
+                    (err, result) => {
+                        if (err) reject(err);
+                        else resolve(result);
+                    }
+                )
+            });
+
+            const [resultA, resultB] = await Promise.race([
+                timeoutPromise,
+                Promise.all([pollServiceA(), serviceB])
+            ]) as [ServiceA_Result, ServiceB_Result, ServiceC_Result];
+
+            return this.d.merge(resultA, resultB)
+
+            // TODO implement other paths
+        } catch (err) {
+            if (!completed && !cancellation.isCancelled()) {
+                errors.push(err as Error);
+                throw await this.errorMapper.error(errors);
+            } else {
+                throw err;
+            }
+        } finally {
+            clearTimeout(timeoutHandler);
+        }
     }
 }
 
@@ -43,8 +99,8 @@ export interface ServiceB {
 	 * @param callback exactly one of Result or Error will be present
 	 */
 	submit(
-        req: Main_Request, 
-        isCancelled: () => boolean, 
+        req: Main_Request,
+        isCancelled: () => boolean,
         timeout: number,
         callback: (err: Error | undefined, result: ServiceB_Result) => void
     ): void;
@@ -53,7 +109,7 @@ export interface ServiceB {
 export type ServiceC_Result = string
 export interface ServiceC {
 
-	call(req: Main_Request): Promise<ServiceC_Result>;	
+	call(req: Main_Request): Promise<ServiceC_Result>;
 }
 
 export interface ServiceD {
@@ -61,7 +117,7 @@ export interface ServiceD {
 	/**
 	 * @return may be completed exceptionally if merging fails
 	 */
-	merge(a: ServiceA_Result, b: ServiceB_Result): Promise<Main_Result>;	
+	merge(a: ServiceA_Result, b: ServiceB_Result): Promise<Main_Result>;
 }
 
 export type ServiceE_Result = string
@@ -73,8 +129,8 @@ export interface ServiceE {
      * @returns future for the result and callback that may be called to abort execution early,
      *          when aborted the future will be rejected by error
      */
-	transform(b: Promise<ServiceB_Result>): [Promise<ServiceE_Result>, ServiceE_CancelToken];	
-    combine(a: Promise<ServiceA_Result>, c: Promise<ServiceC_Result>): [Promise<ServiceE_Result>, ServiceE_CancelToken];	
+	transform(b: Promise<ServiceB_Result>): [Promise<ServiceE_Result>, ServiceE_CancelToken];
+    combine(a: Promise<ServiceA_Result>, c: Promise<ServiceC_Result>): [Promise<ServiceE_Result>, ServiceE_CancelToken];
 }
 
 export interface ServiceF {
